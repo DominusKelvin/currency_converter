3a66c0475e69398e91184169529db82a8c72b67f
Kelvin Omereshone <kelvinomereshone@gmail.com>
2018-06-27 14:04:40 +0100

    Added the function to fetch the calculate the chosen currency conversions

--- public/js/index.js
+++ public/js/index.js
@@ -1,14 +1,17 @@
-document.addEventListener("DOMContentLoaded", fetchCurrencies);
+document.addEventListener("DOMContentLoaded", fetchCurrencies); // adding event listener for when the DOM is loaded
+
 let currencyFrom = document.getElementById('currencyFrom')
 let currencyTo = document.getElementById('currencyTo')
 
+const baseUrl = 'https://free.currencyconverterapi.com/api/v5/'
+document.getElementById('convertButton').addEventListener('click', computeConversion)
 function fetchCurrencies() {
-    fetch('https://free.currencyconverterapi.com/api/v5/currencies')
+    fetch(`${baseUrl}currencies`)
         .then(response => {
             return response.json()
     }).then(function(data) {
         let currencies = data.results
-        populateSelectBoxes(currencies)
+        populateSelectBoxes(currencies) // calling the populateSelectBoxes function
     }).catch(err => console.log(err));
 }
 
@@ -25,4 +28,34 @@ function populateSelectBoxes(currencies) {
         optionTo.value = currency
         currencyTo.appendChild(optionTo)   
     }
+}
+
+function computeConversion(amount=0, fromCurrency, toCurrency, calculateAmount) {
+    fromCurrency = encodeURIComponent(currencyFrom.options[currencyFrom.selectedIndex].value)
+    toCurrency = encodeURIComponent(currencyTo.options[currencyTo.selectedIndex].value)
+    console.log(fromCurrency)
+    console.log(toCurrency)
+    amount = Number(document.getElementById('amount').value)
+    if(!amount) { return }
+    let query = `${fromCurrency}_${toCurrency}`
+    let url =  `${baseUrl}convert?q=${query}&compact=ultra`
+    console.log(url)
+    fetch(url).then(function(response){
+        return response.json()
+    }).then(function(data){
+        console.log(data)
+        let val = data[query]
+        if (val) {
+            let total = val * amount
+            document.getElementById('convertedCurrency').value = total.toFixed(2)
+            calculateAmount(null, Math.round(total * 100) / 100)
+        }
+        else {
+            let err = new Error("Value not found for " + query)
+            console.log(err)
+            calculateAmount(err)
+        }
+    }).catch(err => {
+        console.log(err)
+    })
 }
